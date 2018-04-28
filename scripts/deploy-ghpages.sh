969352b6bd6a52bd0ed202f154379c1b04a36df1
Love Huria <lovehuria@gmail.com>
2018-04-28 22:41:32 +0530

    Test 2 - Circle CI

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -37,7 +37,7 @@ fi
 
 # copy over or recompile the new site
 pwd
-cp -a "/home/ubuntu/lhuria94.github.io/" .
+cp -a /home/ubuntu/lhuria94.github.io/ .
 
 # stage any changes and new files
 git add -A
