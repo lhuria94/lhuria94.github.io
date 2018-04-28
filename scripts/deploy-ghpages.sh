a70508739b9bb23f84687aa02e7bd848b54d4184
Love Huria <lovehuria@gmail.com>
2018-04-28 18:08:42 +0530

    Test 2 - Circle CI

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -27,7 +27,7 @@ then
     git checkout master
     # delete any old site as we are going to replace it
     # Note: this explodes if there aren't any, so moving it here for now
-    git merge dev-1.0
+    git merge origin/dev-1.0
 else
     git checkout --orphan master
 fi
