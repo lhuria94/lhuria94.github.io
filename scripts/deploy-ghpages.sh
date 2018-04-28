c7a5987845caa0f96e741611fededb6ee9c12cbc
Love Huria <lovehuria@gmail.com>
2018-04-29 00:37:38 +0530

    Test 2 - Circle CI

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -38,15 +38,14 @@ fi
 
 # copy over or recompile the new site
 pwd
-echo $(ls -al)
 cp -a /home/ubuntu/lhuria94.github.io/. /home/ubuntu/master-branch/
 
 # stage any changes and new files
-git add -A
+git add .
 # now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
-git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
+git commit -m "Deploy to GitHub pages [ci skip]"
 # and push, but send any output to /dev/null to hide anything sensitive
-git push --force --quiet origin master
+git push --force origin master
 # go back to where we started and remove the gh-pages git repo we made and used
 # for deployment
 cd ..
